30225b4aaaccecf0191bf2f889ccfa91444f898f
unknown <smsarmad92@gmail.com>
2024-04-13 06:17:47 -0700

    editted npm bin path

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-#! usr/bin/env node
+#! /usr/bin/env node
 import inquirer from "inquirer";
 const random_number = Math.floor(Math.random() * 10);
 console.log("Welcome to the number guessing game");
