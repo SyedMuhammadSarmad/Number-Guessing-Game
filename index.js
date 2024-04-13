eb3e612ce56aac662704a9144ce5b79167fc9327
unknown <smsarmad92@gmail.com>
2024-04-13 06:01:18 -0700

    first commit

---
+++ index.js
@@ -0,0 +1,13 @@
+#! usr/bin/env node
+import inquirer from "inquirer";
+const random_number = Math.floor(Math.random() * 10);
+console.log("Welcome to the number guessing game");
+const input = await inquirer.prompt([
+    { "message": "enter any no", "name": "guessed_no", "type": "number" }
+]);
+if (input.guessed_no == random_number) {
+    console.log("Congratulations! you won the game");
+}
+else {
+    console.log("You guessed wrong number!");
+}
